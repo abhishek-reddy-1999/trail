73361200060ff02e9b26d71d1d75cc98172a77ff
abhishek-reddy-1999 <132802759+abhishek-reddy-1999@users.noreply.github.com>
2024-02-19 14:16:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,11 @@
-const express=require('express')
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
 
-var abhi=require('./SQL')
-var session = require('express-session')
-const app=express()
-const port=process.env.port || 4000
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
-abhi()
-
-app.set('trust proxy', 1) // trust first proxy
-app.use(session({
-  secret: 'keyboard cat',
-  resave: false,
-  saveUninitialized: true,
-  cookie: { secure: true }
-}))
-app.get('/', (req, res) => { 
-  res.send('Hello, Azure! This is a Node.js application.'); 
-}); 
-  
-app.listen(port, () => { 
-  console.log(`Server is running on port ${port}`); 
-}); 
-
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
