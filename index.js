113b96988a1c9ceb0e4bc63a3d1d1e6273e086d2
abhishek-reddy-1999 <132802759+abhishek-reddy-1999@users.noreply.github.com>
2024-02-23 09:46:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
+const path =require('path')
+app.use(express.static(path.join(__dirname+"/dist")))
+
 
 app.get('/', (req, res) => {
-  res.send('Hello World!..........App under development...........')
-})
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'))
+});
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
